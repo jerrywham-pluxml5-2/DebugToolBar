2ca89c0c9e5dfc0b7e7cfc1463723e7f43faa9f4
Cyril MAGUIRE <github@ecyseo.net>
2013-10-05 02:36:58 +0200

    Barre de debug disponible sur toutes les pages (publiques et privées)

---
+++ DebugToolBar.php
@@ -0,0 +1,25 @@
+<?php
+class DebugToolBar extends plxPlugin {
+	
+	public function __construct($default_lang) {
+		
+		# appel du constructeur de la classe sigPlugin (obligatoire)
+		parent::__construct($default_lang);
+		
+		# Déclarations des hooks
+		$this->addHook('plxMotorConstructLoadPlugins', 'addDebugToolBar');
+		$this->addHook('AdminFootEndBody', 'printToolBar');
+		$this->addHook('ThemeEndBody', 'printToolBar');
+	}
+	public function addDebugToolBar() {
+		include_once(PLX_PLUGINS.'DebugToolBar/class_outils_debug.php');
+	}
+
+	public function printToolBar() {
+		Debug::getInstance()->printBar();
+	}
+	
+}
+
+
+?>
