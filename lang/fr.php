cdd96db3e42414ed1a9f0739cb437c9d72cbbbad
Cyril MAGUIRE <github@ecyseo.net>
2013-10-09 21:31:33 +0200

    First push

---
+++ lang/fr.php
@@ -0,0 +1,13 @@
+<?php
+
+$LANG = array(
+		'L_TITLE'                    => 'Configuration de la barre de débug',
+		'L_CONFIG_DESCRIPTION'       => 'Permet d\'afficher la barre de débug dans la section administration et/ou publique du site',
+		'L_NO'                       => 'Non',
+		'L_YES'                      => 'Oui',
+		'L_CONFIG_PUBLIC'            => 'Afficher la barre de débug dans la section publique du site ?',
+		'L_BE_CAREFUL_IN_PRODUCTION' => 'Nous vous déconseillons de l\'activer en environnement de production',
+		'L_DEBUG_ACTIVATED_IN_PUBLIC'=> 'Barre de débug activée en section publique',
+		'L_CONFIG_SAVE'              => 'Sauvegarder',
+	);
+?>
